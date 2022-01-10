fec98d6a7ef589c37d790c88c2b1fb3581a96eac
David <dunderwood1396@gmail.com>
2022-01-10 18:41:41 +0000

    subtract two numbers

--- src/test/java/org/example/Calculator/CalculatorTest.java
+++ src/test/java/org/example/Calculator/CalculatorTest.java
@@ -17,4 +17,12 @@ public class CalculatorTest
         int result = calculator.add(2, 3);
         assertEquals(5, result);
     }
+
+    @Test
+    public void subtractTwoNumbers()
+    {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(6, 3);
+        assertEquals(3, result);
+    }
 }
