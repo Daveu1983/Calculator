3178499df500b5f32c8695257e8c51efcf98d3d3
David <dunderwood1396@gmail.com>
2022-01-10 18:39:25 +0000

    added target folder to git ignore and add function to calculator

---
+++ src/main/java/org/example/Calculator/Calculator.java
@@ -0,0 +1,13 @@
+package org.example.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator
+{
+
+    public int add(int i, int i1) {
+        return i + i1;
+    }
+}
---
+++ src/test/java/org/example/Calculator/CalculatorTest.java
@@ -0,0 +1,20 @@
+package org.example.Calculator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest
+{
+    @Test
+    public void addTwoNumbers()
+    {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+}
